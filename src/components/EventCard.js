a973a6fedd0615b4024c9c92a5862fde0f394e69
ViDA_TG <victorademiju.t@gmail.com>
2023-01-16 13:13:53 +0100

    Complete and delete

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom"
+import supabase from "../config/SupabaseConfig"
 
-const EventCard = ({event}) => {
+const EventCard = ({event, onDeleted}) => {
 
+    const handleDelete = async () => {
+        const { error } = await supabase
+            .from('events')
+            .delete()
+            .eq("id", event.id)
+        
+        if(!error) onDeleted()
+    }
     return (
         <div className="event-card">
             <h3>{event.name}</h3>
@@ -12,7 +21,7 @@ const EventCard = ({event}) => {
                 <Link to={"/" + event.id}>
                     <i className="material-icons">edit</i>
                 </Link>
-                <i className="material-icons">delete</i>
+                <i className="material-icons" onClick={handleDelete}>delete</i>
             </div>
         </div>
     )
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,21 +8,21 @@ const HomePage = () => {
 
     const [ events, setEvents ] = useState(null)
     const [ fetchError, setFetchError ] = useState(null)
+    
+    const fetchEvents = async () => {
+        const { data, error } = await supabase
+            .from("events")
+            .select()
+
+        if (error){
+            setFetchError("Error getting events")
+        }
+        if (data){
+            setEvents(data)
+        }
+    }
 
     useEffect(() =>{
-        const fetchEvents = async () => {
-            const { data, error } = await supabase
-                .from("events")
-                .select()
-
-            if (error){
-                setFetchError("Error getting events")
-            }
-            if (data){
-                setEvents(data)
-            }
-        }
-
         fetchEvents()
     }, [])
     
@@ -30,7 +30,7 @@ const HomePage = () => {
         <div className="page home">
             { fetchError && (<p>{fetchError}</p>) }
             <div className="event-grid">
-                { events && events.map((event) => <EventCard key={event.id} event={event} />)}
+                { events && events.map((event) => <EventCard key={event.id} event={event} onDeleted={fetchEvents}/>)}
             </div>
         </div>
     )
