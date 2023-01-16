0e6e7531dfc145aeb6cf5a3a70acd77acf8b3294
ViDA_TG <victorademiju.t@gmail.com>
2023-01-16 08:42:46 +0100

    Create events

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/Homepage';
+import CreateTask from './pages/CreateTask';
 
 function App() {
   return (
     <BrowserRouter className="App">
       <nav className="App-header">
         <Link to="/" >Home</Link>
+        <Link to="/new-event" >New Event</Link>
       </nav>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/new-event' element={<CreateTask />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/CreateTask.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const CreateTask = () => {
+    const [ name, setName ] useState(null)
+    const [ description, setDescription ] useState(null)
+    const [ rating, setRating ] useState(null)
+
+    const handleCreateTask = () => {
+
+    }
+
+    return (
+        <div className="page create">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Event Name:</label>
+                <input 
+                type="text" 
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+                
+                <label htmlFor="description">Method:</label>
+                <textarea 
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                />
+
+                <label htmlFor="rating">Rating:</label>
+                <input 
+                type="number"
+                id="rating"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                />
+
+            </form>
+        </div>
+    )
+}
+
+export default CreateTask;
