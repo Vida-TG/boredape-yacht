2b73c2f427daa6bf32b3cf93325a533ef5bd758f
ViDA_TG <victorademiju.t@gmail.com>
2023-01-20 03:42:34 +0100

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,19 @@
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react"
+import { useNavigate } from "react-router-dom";
 import { supabase } from "../config/SupabaseConfig";
 
 const Login = () => {
-    supabase.auth.onAuthStateChange
+    const navigate = useNavigate()
+    supabase.auth.onAuthStateChange(async (e) => {
+        if (e !== "SIGNED_OUT") {
+            navigate("/success")
+        } else {
+            navigate("/success")
+        }
+    })
+
+    return (
+    )
 }
 
 export default Login;
